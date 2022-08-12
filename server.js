4ec1aa16061d284882d7c48145fbb372a107c9be
Jawid Noori <Hajikaka4030@gmail.com>
2022-08-11 17:35:48 -0700

    routes added succesfully

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const routes = require('./routes')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(routes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-bond-api', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+app.use(require('./routes'));
 
-mongoose.set('debug', true)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network');
 
-app.listen(PORT, () => console.log(`App is listening at http://localhost:${PORT}}`))
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`App is listening on localhost:${PORT}`))
